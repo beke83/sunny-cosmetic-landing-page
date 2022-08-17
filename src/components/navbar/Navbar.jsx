16acd0730eaa3d5f8e6d0464c9b935f5b451e139
beke83 <bekebenjamin@yahoo.com>
2022-08-17 13:52:56 +0100

    Features section in progress

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faBasketShopping, faUser, } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faBasketShopping, faUser, faXmark, faBars } from '@fortawesome/free-solid-svg-icons'
+
+const Menu = () => {
+  return (
+    <>
+      <p><a href="#home">Home <span><div className='rectangle' /></span></a></p>
+      <p><a href='#about'>About Us</a></p>
+      <p><a href="products">Products</a></p>
+      <p><a href='#faq'>Faqs</a></p>
+      <p><a href="contact">Contact Us</a></p>
+    </>
+  );
+}
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='navbar'>
       <div className='navbar-logo'>
@@ -12,11 +26,7 @@ const Navbar = () => {
       </div>
       <div className='navbar-items'>
         <div className="navbar-links-container">
-          <p><a href="#home">Home <span><div className='rectangle' /></span></a></p>
-          <p><a href='#about'>About Us</a></p>
-          <p><a href="products">Products</a></p>
-          <p><a href='#faq'>Faqs</a></p>
-          <p><a href="contact">Contact Us</a></p>
+          <Menu />
         </div>
       </div>
       <div className="navbar-info">
@@ -27,6 +37,35 @@ const Navbar = () => {
           <span className="basket-item-qty">2</span>
         </div>
         <div><FontAwesomeIcon icon={faUser} /></div>
+      </div>
+      <div className="navbar-menu">
+        {toggleMenu ? (
+          <FontAwesomeIcon icon={faXmark} size='2x' onClick={() => setToggleMenu(false)}
+            className='flip-scale-down-ver' />
+        ) : (
+          <FontAwesomeIcon icon={faBars} size='2x'
+            onClick={() => setToggleMenu(true)}
+            className='flip-scale-down-ver'
+          />
+        )}
+        {
+          toggleMenu && (
+            <div className="navbar-menu-container scale-up-center">
+              <div className="navbar-menu-container-links">
+                <Menu />
+              </div>
+              <div className="navbar-menu-info">
+                <div>
+                  <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </div>
+                <div className="basket-icon"><FontAwesomeIcon icon={faBasketShopping} />
+                  <span className="basket-item-qty">2</span>
+                </div>
+                <div><FontAwesomeIcon icon={faUser} /></div>
+              </div>
+            </div>
+          )
+        }
       </div>
     </div>
   )
--- src/pages/feature/Feature.jsx
+++ src/pages/feature/Feature.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './feature.css';
+import ProductOne from '../../assets/product1.svg';
 
 const Feature = () => {
   return (
-    <div>
-      Feature
+    <div className='feature feature-padding' id="about">
+      <div className="feature-heading">
+        <h2>Best Cosmetic Products</h2>
+        <p>Launched in 2002, Makeup City has brought renowned international personal care brands under one roof for the first time in Pakistan. from a journey of a few stores, we have grown significantly, by making our brands available to almost every where a person can think off.</p>
+      </div>
+      <div className="feature-product-details-container">
+        <div className="feature-product-image">
+          <img src={ProductOne} alt="product-img" />
+        </div>
+        <div className="feature-product-details">
+          <h3>GOS- Primer Plus 30 ML</h3>
+          <p>GOSH COPENHAGEN offers a wide selection of products in different categories: Cosmetics, Fragrances, Hair Care, Face Care, Body Care. GOSH Cosmetics are available online. </p>
+          <p>Load More</p>
+        </div>
+
+      </div>
     </div>
   )
 }
