5c9feb6ce67d1920c83b806ffc9c805e1187e07d
beke83 <bekebenjamin@yahoo.com>
2022-08-29 16:10:25 +0100

    fixes

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,21 +2,31 @@ import React from 'react';
 import './cta.css';
 
 import CTAImage from '../../assets/cta-img.svg';
+import { Fade, Slide, Zoom } from 'react-reveal';
+import Wobble from 'react-reveal/Wobble';
 
 const CTA = () => {
 
   return (
     <div className="cta-banner">
       <div className="cta-text">
-        <h3>Enough talk, let's make you look beautiful</h3>
-        <p>There are many variations of to this product and you will love it all.</p>
+        <Fade bottom>
+          <h3>Enough talk, let's make you look beautiful</h3>
+        </Fade>
+        <Slide right>
+          <p>There are many variations of to this product and you will love it all.</p>
+        </Slide>
       </div>
-      <div className="cta-img">
-        <img src={CTAImage} alt="product-image" />
-      </div>
-      <div className="cta-button">
-        <button>Get started</button>
-      </div>
+      <Zoom>
+        <div className="cta-img">
+          <img src={CTAImage} alt="product-image" />
+        </div>
+      </Zoom>
+      <Wobble>
+        <div className="cta-button">
+          <button>Get started</button>
+        </div>
+      </Wobble>
     </div>
   )
 }
